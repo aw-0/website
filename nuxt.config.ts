f1972e8643e0125401877132565eaebae75aeadc
Andrew <43796708+aw-0@users.noreply.github.com>
2022-06-20 21:47:29 -0500

    feat(meta): Add og:title & theme-color

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,9 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     meta: {
-        title: "andreww.co",
+        title: 'andreww.co',
+        'og:title': 'andrew.co',
+        'theme-color': '#CDEEFF',
         link: [
             {
                 href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css',
