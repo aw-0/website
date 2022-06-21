118944460b94dc2e66396cf6031d5ba4a8a5fde6
Andrew <43796708+aw-0@users.noreply.github.com>
2022-06-20 21:57:35 -0500

    fix(meta): Fix meta tags again

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,16 +2,19 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    meta: [
-        { name: 'title', content: 'andreww.co' },
-        { name: 'og:title', content: 'andrew.co' },
-        { name: 'theme-color', content: '#CDEEFF' },
-        {
-            name: 'link',
-            href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css',
-            rel: 'stylesheet',
-        }
-    ],
+    head: {
+        meta: [
+            { name: 'title', content: 'andreww.co' },
+            { name: 'og:title', content: 'andreww.co'},
+            { name: 'theme-color', content: '#CDEEFF'},
+        ],
+        link: [
+            {
+                href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css',
+                rel: 'stylesheet',
+            }
+        ]
+    },
     buildModules: [
         '@nuxtjs/tailwindcss'
     ],
